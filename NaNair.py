d82211a187d108ec40c9ce9b6008100f9a27e859
Róslín <roslin19@ru.is>
2019-12-02 16:22:29 +0000

    Update

--- NaNair.py
+++
@@ -1,3 +0,0 @@
-class Chuck_Norris():
-    def __init__(self,Chuck_Norris):
-        self.__Chuck_norris = Chuck_Norris
---
+++ NaNairBEINAGRIND.py
@@ -0,0 +1,266 @@
+class AiplaneUI():
+    def __init__(self,LLAPI):
+        pass
+
+    def addPlane(self):
+        pass
+
+    def changePlane(self):
+        pass
+
+    def displayPlane(self):
+        pass
+
+class VoyageUI():
+    def __init__(self,LLAPI):
+        pass
+
+    def addVoyage(self):
+        pass
+
+    def changeVoyage(self):
+        pass
+
+    def displayVoyage(self):
+        pass
+
+class employeeUI():
+    def __init__(self,LLAPI):
+        pass
+
+    def addEmployee(self):
+        pass
+
+    def changeEmployee(self):
+        pass
+
+    def displayEmployee(self):
+        pass
+
+class flightUI():
+    def __init__(self,LLAPI):
+        pass
+
+    def addFlight(self):
+        pass
+
+    def changeFligth(self):
+        pass
+
+    def displayFlight(self):
+        pass
+
+class destinationUI():
+    def __init__(self,LLAPI):
+        pass
+
+    def addFlight(self):
+        pass
+
+    def addDestination(self,LLAPI):
+        pass
+
+    def changeDestination(self,LLAPI):
+        pass
+
+    def displayDestination(self,LLAPI):
+        pass
+
+
+class Airplane():
+    def __init__(self):
+        pass
+
+class Voyage():
+    def __init__(self):
+        pass
+
+class employee():
+    def __init__(self):
+        pass
+
+class flight():
+    def __init__(self):
+        pass
+
+class destination():
+    def __init__(self):
+        pass
+
+class AirplaneLL(Airplane):
+    def __init__(self):
+        pass
+
+    def get_type(self):
+        pass
+
+    def get_typeID(self):
+        pass
+
+    def get_planeInsignia(self):
+        pass
+
+    def get_model(self):
+        pass
+
+    def get_capacity(self):
+        pass
+
+    def get_emptyWeight(self):
+        pass
+
+    def get_maxTakeoffWeight(self):
+        pass
+
+    def get_unitThrust(self):
+        pass
+
+    def get_serviceCeiling(self):
+        pass
+    
+    def get_length(self):
+        pass
+
+    def get_height(self):
+        pass
+
+    def get_wingSpan(self):
+        pass
+
+    def validateInfo(self):
+        pass
+
+class voyageLL(voyage):
+    def __init__(self):
+        pass
+
+    def get_flight(self):
+        pass
+
+    def get_homeflight(self):
+        pass
+
+    def validateInfo(self):
+        pass
+
+class EmployeeLL(employee):
+
+    def __init__(self):
+        pass
+
+    def get_name(self):
+        pass
+
+    def get_ID(self):
+        pass
+
+    def get_address(self):
+        pass
+
+    def get_phoneNumber(self):
+        pass
+
+    def get_gsm(self):
+        pass
+
+    def get_email(self):
+        pass
+
+    def validateInfo(self):
+        pass
+
+class flightLL(flight):
+    def __init__(self):
+        pass
+
+    def get_departingFrom(self):
+        pass
+    
+    def get_departure(self):
+        pass
+
+    def get_arrivingAt(self):
+        pass
+
+    def get_arrival(self):
+        pass
+
+    def validateInfo(self):
+        pass
+
+class destinationLL(destination):
+    def __init__(self):
+        pass
+
+    def get_country(self):
+        pass
+
+    def get_airport(self):
+        pass
+
+    def get_flightTime(self):
+        pass
+
+    def get_distance(self):
+        pass
+
+    def get_emergencyContact(self):
+        pass
+
+    def get_emergencyPhone(self):
+        pass
+
+    def validateInfo(self):
+        pass
+
+
+class AirplaneIO():
+    def __init__(self):
+        pass
+
+    def loadAirplaneFromFile(self):
+        pass
+
+    def storeAirplaneToFile(self):
+        pass
+
+class VoyageIO():
+    def __init__(self):
+        pass
+
+    def loadVoyageFromFile(self):
+        pass
+
+    def StoreVoyageToFile(self):
+        pass
+
+class employeeIO():
+    def __init__(self):
+        pass
+
+    def loadEmployeeFromFile(self):
+        pass
+
+    def storeEmployeeToFile(self):
+        pass
+
+class flightIO():
+    def __init__(self):
+        pass
+
+    def loadFlightFromFile(self):
+        pass
+
+    def storeFlightToFile(self):
+        pass
+
+class LLAPI():
+    def __init__(self):
+        pass
+
+class IOAPI():
+    def __init__(self):
+        pass
+
+
+
