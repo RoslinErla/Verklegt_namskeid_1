29618646d1340efa1f2da8c1654d2e5483d25b4b
Róslín <roslin19@ru.is>
2019-12-03 12:20:03 +0000

    update

---
+++ 2_employee.py
@@ -0,0 +1,45 @@
+class Employee_Model():
+
+    def __init__(self,name,ID,address,phone_number,email):
+        self.__name = name
+        self.__id= ID
+        self.__address = address
+        self.__phone_number = phone_number
+        self.__email = email
+
+    def __str__(self):
+        pass
+
+
+class EmployeeLL(Employee_Model):
+
+    def __init__(self):
+        # self.__IOAPI = IOAPI()
+        pass
+
+    def add_employee(self):
+        pass
+
+    def change_employee(self):
+        pass
+
+    def get_name(self):
+        pass
+
+    def get_ID(self):
+        pass
+
+    def get_address(self):
+        pass
+
+    def get_phoneNumber(self):
+        pass
+
+    def get_email(self):
+        pass
+
+    def get_flight(self):
+        pass
+
+    def validateInfo(self):
+        pass
