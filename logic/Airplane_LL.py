c1e2578cd011657db7aabe622c6360f25b45efea
arnare19 <arnare19@ru.is>
2019-12-04 12:24:49 +0000

    update

--- logic/Airplane_LL.py
+++ logic/Airplane_LL.py
@@ -5,6 +5,8 @@ class Airplane_LL(Airplane):
     def __init__(self, inp):
         self.inp = inp
     
+plane_type, type_ID, plane_insignia, model
+
     def validate_info(self):
         if type(self.inp) == str:
             for word in self.inp:
