2bbfaf085175052886c5085fd38840e472a32e61
Berglind Anna <berglindav@gmail.com>
2019-12-04 12:41:25 +0000

    Flight_LL updated

---
+++ logic/Flight_LL.py
@@ -0,0 +1,54 @@
+from model.flightM import Flight
+import string
+
+
+class FlightLL(Flight):
+
+    def __init__(self):
+        # self.__IOAPI = IOAPI()
+        pass
+
+    def add_flight(self):
+        pass
+
+    def change_flight(self):
+        pass
+
+    def validate_departing_from(self):
+        pass
+
+    def validate_departure_time(self):
+        pass
+
+    def validate_arriving_at(self):
+        pass
+    
+    def validate_arrival_time(self):
+        pass
+
+    def validate_aircraft_ID(self):
+        pass
+
+    def validate_captain(self):
+        pass
+
+    def validate_co_pilot(self):
+        pass
+
+    def validate_fsm(self):
+        pass
+
+    def validate_fa1(self):
+        pass
+
+    def validate_fa2(self):
+        pass
+
+    def validate_flight_number(self):
+        pass
+
+
+
+
+
+
--- runmoforun.py
+++ runmoforun.py
@@ -2,3 +2,7 @@ from model.AirplaneM import Airplane
 from logic.Airplane_LL import Airplane_LL
 alpha = Airplane_LL("10")
 alpha.validate_info()
+
+
+
+from model.flightM import Flight
