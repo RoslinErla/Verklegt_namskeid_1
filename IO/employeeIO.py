0f9f28e6dae70ec8eef78677df78ae3b0612a90d
Róslín <roslin19@ru.is>
2019-12-04 22:36:10 +0000

    gerði eitt stykki IO klasa. eða allavega byrjaði á því

---
+++ IO/employeeIO.py
@@ -0,0 +1,18 @@
+from model.employeeM import Employee
+
+
+class employeeIO:
+    EMPLOYEE_FILE = "./files/employee.py"
+
+    def __init__(self):
+        pass
+
+    def load_employee_from_file_alphabetically(self):
+        pass
+
+    def load_employee_from_file_by_status(self):
+        pass
+
+    def Add_employee_to_file(self,name, ssn, phone, user_name, rank, permits, status):
+        pass
+
