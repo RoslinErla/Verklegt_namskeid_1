f46f0cba6f8a86df56b4073513908e34070318ab
Róslín <roslin19@ru.is>
2019-12-04 15:52:56 +0000

    Employee_LL update

--- logic/Employee_LL.py
+++ logic/Employee_LL.py
@@ -1,10 +1,17 @@
-from model.employeeM import Employee
+from model.EmployeeM import Employee
+import string
 
-class EmployeeLL(Employee):
-
+class Employee_LL():
     def __init__(self):
         # self.__IOAPI = IOAPI()
-        pass
+        self.__name = ""
+        self.__ssn= ""
+        self.__address = ""
+        self.__phone_number = "" 
+        self.__email = ""
+        self.__rank = ""
+        self.__permit = ""
+        self.__status = ""
 
     def add_employee(self):
         pass
@@ -12,6 +19,18 @@ class EmployeeLL(Employee):
     def change_employee(self):
         pass
 
-
-    def validateInfo(self):
-        pass
+    def validate_name(self,name):
+        """Checks if name is valid and returns an error message if its not"""
+        name = name.split()
+        for elements in name:
+            for letter in elements:
+                if letter.isdigit() or letter in string.punctuation:
+                    return "{} is invalid".format(name)
+        self.__name = name
+    
+    def validate_ssn(self,ssn):
+        """Checks if the ssn is valid. Returns an error message if it is not"""
+        if type(ssn) == int and len(str(ssn)) == 10:
+            self.__ssn = ssn
+        else:
+            print("{} is invalid".format(ssn))
