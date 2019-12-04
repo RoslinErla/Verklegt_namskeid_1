f2c908d3d70c5655cfec4d39467ceaa2a83abc19
arnare19 <arnare19@ru.is>
2019-12-04 15:04:32 +0000

    update

--- logic/Airplane_LL.py
+++ logic/Airplane_LL.py
@@ -1,17 +1,16 @@
 from model.AirplaneM import Airplane
 import string
 
-class Airplane_LL(Airplane):
+class AirplaneLL(Airplane):
     def __init__(self, inp):
         self.inp = inp
 
-    # def make_inp_dict(self):
-    #     inp_dict = {}
-    #     split_stripped = self.inp.strip().split(", ")
-    #     inp_dict(split_stripped[0]) = validate_manufacturer(split_stripped[0])
-    #     inp_dict(split_stripped[1]) = validate_typeID(split_stripped[1])
-    #     inp_dict(split_stripped[2]) = validate_plane_insignia(split_stripped[2])      
-    #     inp_dict(split_stripped[3]) = model(split_stripped[3])
+    def make_inp_dict(self):
+        split_stripped = self.inp.strip().split(", ")
+        validate_manufacturer(split_stripped[0])
+        validate_typeID(split_stripped[1])
+        validate_plane_insignia(split_stripped[2])      
+        model(split_stripped[3])
 
     def validate_manufacturer(self):
         if type(self.inp) == str:
@@ -29,11 +28,12 @@ class Airplane_LL(Airplane):
 
     def validate_plane_insignia(self):
         if type(self.inp) == str:
-            for word in self.inp:
-                if word != "TF-":
-                    for letter in word:
-                        if letter.isalpha:
-                            return True
+            for num, letter in enumerate(self.inp):
+                if letter != "-" and num == 2:
+                    if letter.isalpha() == False:
+                        return False                
+        return True
+
 
     def validate_model(self):
         if type(typeID) == str:
