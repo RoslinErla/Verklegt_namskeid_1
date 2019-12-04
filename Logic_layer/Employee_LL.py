969a28447fb41c7d9dc89ce8d175d216774da0d7
Róslín <roslin19@ru.is>
2019-12-04 10:24:17 +0000

    split the 2_employee file into employee_model and employee_LL

--- Logic_layer/Employee_LL.py
+++ Logic_layer/Employee_LL.py
@@ -1,16 +1,3 @@
-class Employee_Model():
-
-    def __init__(self,name,ID,address,phone_number,email):
-        self.__name = name
-        self.__id= ID
-        self.__address = address
-        self.__phone_number = phone_number
-        self.__email = email
-
-    def __str__(self):
-        pass
-
-
 class EmployeeLL(Employee_Model):
 
     def __init__(self):
