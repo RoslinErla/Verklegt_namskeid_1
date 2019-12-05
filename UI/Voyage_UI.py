15b51f61abb124786a6de5d4dfce858297f6fc7f
Berglind Anna <berglindav@gmail.com>
2019-12-05 15:52:03 +0000

    Voyage_UI updated

--- UI/Voyage_UI.py
+++ UI/Voyage_UI.py
@@ -1,5 +1,5 @@
-from logic.Voyage_LL import VoyageLL
-from model.VoyageM import Voyage
+"""from logic.Voyage_LL import VoyageLL
+from model.VoyageM import Voyage"""
 
 
 
@@ -8,7 +8,7 @@ class VoyageUi:
     def __init__(self):
         self.__ll_voyage = VoyageLL()
 
-    def create_flight(self):   # Bæta við flugi ... 
+    def create_flight(self):   # Bæta við ferð ... ?
         pass
 
 
@@ -22,26 +22,55 @@ class VoyageUi:
             print("Enter b to go back and q to got to the main menu.")
            
 
-            action = input("Please enter your command").lower()
+            action = input("Please enter your command: ").lower()
 
             if action == 1:
-                old_voyage = print("\tEnter 1 to repeat an old voyage")
-                new_voyage = print("\tEnter 2 to create a new voyage")
-                new_destination = print("\tEnter 3 to create a new destination")
-                create_
+                create_voyage = create_menu()
             
             if action == 2:
-                change_voyage_state = print("\tEnter 1 to change voyage state)
-                change_emergency_contact = print("\tEnter 2 to change emergency contact's name and/or phone number")
+                change_voyage = change_menu()
             
             if action == 3:
-                daily_voyages_list = print("\tEnter 1 for a daily list of voyages")
-                weakly_voyages_list = print("\tEnter 2 for a weakly list of voyages")
-                flight_number_list = print("\tEnter 3 for a list of flight numbers")
-                voyage_sate = print("\tEnter 4 to see the state of the voyages for today")
+                display_voyage = display_menu()
 
 
-                new_input = input("What you wanna do?: ")
+    
+    def create_menu(self):
+        action_create = ""
+        while(action != "q" or "b"):
+            print("\tEnter 1 to repeat an old voyage")
+            print("\tEnter 2 to create a new voyage")
+            print("\tEnter 3 to create a new destination")
+
+            action_create = input("Please enter your commmand: ")
+        
+        return action_create
+
+        
+    
+    def change_menu(self):
+        action_change = ""
+        while(action != "q" or "b"):
+            print("\tEnter 1 to change voyage state")
+            print("\tEnter 2 to change emergency contact's name and/or phone number")
+
+            action_change = input("Please enter your commmand: ")
+        
+        return action_change
+
+
+    def display_menu(self):
+        action_display = ""
+        while(action != "q" or "b"):
+            print("\tEnter 1 for a daily list of voyages")
+            print("\tEnter 2 for a weakly list of voyages")
+            print("\tEnter 3 for a list of flight numbers")
+            print("\tEnter 4 to see the state of the voyages for today")
+            action_display = input("Please enter your commmand: ")
+        
+        return action_display
+
+
 
             
 
