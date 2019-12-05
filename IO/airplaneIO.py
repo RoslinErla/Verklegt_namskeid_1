f4f2ebc32b75123966960ff4debe2be323226d15
Róslín <roslin19@ru.is>
2019-12-05 12:38:06 +0000

    update

---
+++ IO/airplaneIO.py
@@ -0,0 +1,16 @@
+from model.AirplaneM import Airplane
+
+class AirplaneIO:
+    AIRPLANE_FILE = "./files/airplane.csv"
+
+    def __init__(self):
+        pass
+
+    def load_airplane_from_file_alphabetically(self):
+        pass
+
+    def load_airplane_from_file_by_use(self):
+        pass
+
+    def Add_airplane_to_file(self,manufacturer, type_ID, plane_insignia, model):
+        pass
--- IO/employeeIO.py
+++ IO/employeeIO.py
@@ -1,8 +1,9 @@
 from model.employeeM import Employee
+import csv
 
 
 class employeeIO:
-    EMPLOYEE_FILE = "./files/employee.py"
+    EMPLOYEE_FILE = "./files/employee.csv"
 
     def __init__(self):
         pass
@@ -14,5 +15,7 @@ class employeeIO:
         pass
 
     def Add_employee_to_file(self,name, ssn, phone, user_name, rank, permits, status):
-        pass
+        with open(self.EMPLOYEE_FILE, "a", newline = " ") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([name,ssn,phone, user_name, rank, permits, status])
 
