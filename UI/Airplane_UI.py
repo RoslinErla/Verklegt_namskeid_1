b71402f61fec851e5b25c22716c60a229ef7490b
arnare19 <arnare19@ru.is>
2019-12-05 15:46:11 +0000

    update

--- UI/Airplane_UI.py
+++ UI/Airplane_UI.py
@@ -2,13 +2,14 @@ from model.AirplaneM import Airplane
 from logic.Airplane_LL import AirplaneLL
 
 class AirplaneUI:
-    def __init__(self, inp):
-        self.__inp = inp
+    def __init__(self):
+        self.__airplane = Airplane
+        self.__airplanell = AirplaneLL
 
     def airplane_menu(self):
         action = ""
         while action != "b" or action != "q":
-            print("\tAirplane meanu")
+            print("\tAirplane menu")
             print()
             print('The following actions are possible:')
             print('\t1. enter "1" to create a new plane within the system.')
@@ -16,13 +17,31 @@ class AirplaneUI:
             print('\t3. enter "3" to display planes within the system.')
             print('Enter "b" to go back and "q" to got to the main menu.')
 
-            action = input("Please enter your command").lower()
+            action = input("Please enter your command: ").lower()
 
-            if action == 1:
-                manufacturer = input("Enter the manufacturer: ").lower()
-                type_ID = input("Enter the type ID: ").lower()
-                plane_insignia = input("Enter the plane insignia: ").lower()
-                model = input("Enter the model:").lower()
-                if manufacturer.validate_manufacturer == True and type_ID.validate_typeID == True and plane_insignia.validate_plane_insignia:
+            if action == "1":
+                new_plane = self.create_plane()
 
-                new 
+    def create_plane(self):
+        new_plane = []
+        manufacturer = input("Enter the manufacturer: ").lower()
+        while manufacturer.validate_manufacturer == False:
+            print("{} is invalid").format(manufacturer)
+            manufacturer = input("Enter the manufacturer: ").lower()
+
+        type_ID = input("Enter the type ID: ").lower()
+        while type_ID.validate_typeID == False:
+            print("{} is invalid").format(type_ID)
+            type_ID = input("Enter the type ID: ").lower()
+
+        plane_insignia = input("Enter the plane insignia: ").lower()
+        while plane_insignia.validate_plane_insignia == False:
+            print("{} is invalid").format(plane_insignia)
+            plane_insignia = input("Enter the plane insignia: ").lower()
+
+        model = input("Enter the model: ").lower()
+        while model.validate_model == False:
+            print("{} is invalid").format(model)
+            model = input("Enter the model: ").lower()
+        new_plane.append(manufacturer, type_ID, plane_insignia, model)
+        return new_plane
