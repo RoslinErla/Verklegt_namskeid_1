1553d614bea0191799197a7fc64b680400795b45
Berglind Anna <berglindav@gmail.com>
2019-12-05 12:37:04 +0000

    Flight deleted

--- logic/Voyage_LL.py
+++ logic/Voyage_LL.py
@@ -21,6 +21,58 @@ class VoyageLL(Voyage):
     def validate_flight_home(self):
         pass
 
+    # Here comes validations form the former Flight_LL class
+
+    def validate_departing_from(self, departing_from):   # Check if string only has alphabetical letters and is 3 letters long
+        if type(departing_from) == str:
+            for word in departing_from:
+                for letter in word:
+                    if letter.isalpha and len(self.inp) == 3:
+                        return True
+
+    #def validate_departure_time(self):  # Þarf líklega ekki að validate-a, útfært sem datetime í UI
+        #pass
+
+    def validate_arriving_at(self, arriving_at):  # Check if string only has alphabetical letters and is 3 letters long
+        if type(arriving_at) == str:
+            for word in arriving_at:
+                for letter in word:
+                    if letter.isalpha and len(self.inp) == 3:
+                        return True
+    
+    #def validate_arrival_time(self): # Þarf líklega ekki að validate-a, útfært sem datetime í UI
+        #pass
+
+    # def validate_aircraft_ID(self):  Þarf líklegast að import-a frá Airplane_LL
+        # pass
+
+    def validate_captain(self, captain):  # Check if the id is 10 numbers
+        if len(captain) == 10:
+            return True
+
+
+    def validate_co_pilot(self, co_pilot): # Check if the id is 10 numbers
+        if len(co_pilot) == 10:
+            return True
+
+    def validate_fsm(self, fsm): # Check if the id is 10 numbers
+        if len(fsm) == 10:
+            return True
+        
+
+    def validate_fa1(self, fa1): # Check if the id is 10 numbers
+        if len(fa1) == 10:
+            return True
+
+    def validate_fa2(self, fa2): # Check if the id is 10 numbers
+        if len(fa2) == 10:
+            return True
+
+    def validate_flight_number(self, flight_number): # Check if the id is 10 numbers
+        if len(flight_number) == 10:
+            return True
+
+
 
 
     
