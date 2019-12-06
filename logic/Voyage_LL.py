aa605a784b4743e2294de81dd3e40d2e51598ba7
Berglind Anna <berglindav@gmail.com>
2019-12-06 15:54:28 +0000

    Voyage_LL updated docstrings added

--- logic/Voyage_LL.py
+++ logic/Voyage_LL.py
@@ -5,8 +5,7 @@ import string
 class VoyageLL(Voyage):
 
     def __init__(self):
-        pass
-    
+        pass  
 
     def create_voyage(self):
         pass
@@ -26,27 +25,29 @@ class VoyageLL(Voyage):
     # Here comes validations form the former Flight_LL class
 
     def validate_departing_from(self, departing_from):   # Check if string only has alphabetical letters and is 3 letters long
-        if type(departing_from) == str:
-            if len(departing_from) == 3:
-                for letter in departing_from:
-                    if letter.isdigit:
-                        return False
-            else:
-                return False            
+        """ Validates whether "departing_from" is 3 letters long (e.g KEF) and that every letter is a part of the alphabet"""
+        if len(departing_from) == 3:
+            for letter in departing_from:
+                if letter.isdigit or letter in string.punctuation:
+                    return False
+        else:
+            return False
         return True
+    
                     
     #def validate_departure_time(self):  # Þarf líklega ekki að validate-a, útfært sem datetime í UI
         #pass
 
     def validate_arriving_at(self, arriving_at):  # Check if string only has alphabetical letters and is 3 letters long
-        if type(arriving_at) == str:
-            if len(arriving_at) == 3:
-                for letter in arriving_at:
-                    if letter.isdigit:
-                        return False
-            else:
-                return False            
+        """ Validates whether "arriving_at" is 3 letters long (e.g KEF) and that every letter is a part of the alphabet"""
+        if len(arriving_at) == 3:
+            for letter in arriving_at:
+                if letter.isdigit or letter in string.punctuation:
+                    return False
+        else:
+            return False
         return True
+       
     
     #def validate_arrival_time(self): # Þarf líklega ekki að validate-a, útfært sem datetime í UI
         #pass
@@ -55,31 +56,36 @@ class VoyageLL(Voyage):
         # pass
 
     def validate_captain(self, captain):  # Check if the id is 10 numbers
+        """ Validates that the captain's SSN is 10 letters long"""
         if len(captain) == 10:
             return True
        
 
 
     def validate_co_pilot(self, co_pilot): # Check if the id is 10 numbers
+        """ Validates that the co-pilot's SSN is 10 letters long"""
         if len(co_pilot) == 10:
             return True
         
 
     def validate_fsm(self, fsm): # Check if the id is 10 numbers
+        """ Validates that the flight service manager's SSN is 10 letters long"""
         if len(fsm) == 10:
             return True
         
         
-
     def validate_fa1(self, fa1): # Check if the id is 10 numbers
+        """ Validates that the flight servant's SSN is 10 letters long"""
         if len(fa1) == 10:
             return True
 
     def validate_fa2(self, fa2): # Check if the id is 10 numbers
+        """ Validates that the flight servant's SSN is 10 letters long"""
         if len(fa2) == 10:
             return True
 
     def validate_flight_number(self, flight_number): # Check if the id is 10 numbers
+        """ Validates that the flight number is 10 letters long"""
         if len(flight_number) == 10:
             return True
 
