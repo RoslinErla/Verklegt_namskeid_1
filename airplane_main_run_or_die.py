5503da39fff83bc3739c845d2b13251ba0c2a4c8
arnare19 <arnare19@ru.is>
2019-12-06 14:47:28 +0000

    update

--- airplane_main_run_or_die.py
+++ airplane_main_run_or_die.py
@@ -1,6 +1,6 @@
 from model.AirplaneM import Airplane
 from logic.Airplane_LL import AirplaneLL
-from ui.Airplane_UI import AirplaneUI
+from UI.Airplane_UI import AirplaneUI
 
 airui = AirplaneUI()
 airui.airplane_menu()
