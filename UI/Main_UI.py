04ab97f9ae710ea6c9222acefc82de0e5a9fa7c4
arnare19 <arnare19@ru.is>
2019-12-06 17:33:47 +0000

    update

--- UI/Main_UI.py
+++ UI/Main_UI.py
@@ -1,9 +1,15 @@
 from UI.Airplane_UI import AirplaneUI
+# from UI.Destination_UI import DestinationUI
+# from UI.Employee_UI import EmployeeUI
+# from UI.Voyage_UI import VoyageUI
 
 class MainUI():
     def __init__(self):
         self.airplaneui = AirplaneUI()
-    
+        # self.destinationui = DestinationUI()
+        # self.employeeui = Employee_UI()
+        # self.voyageui = VoyageUI()
+
     def main_menu(self):
         action = ""
         while True:
@@ -11,8 +17,10 @@ class MainUI():
             print()
             print('The following actions are possible:')
             print('\t1. Open Airplane Menu.')
-            print('\t2. Open Voyage Menu.')
+            print('\t2. Open Destination Menu.')
             print('\t3. Open Employee Menu.')
+            print('\t4. Open Voyage Menu.')
+
             print('Enter "q" to quit the program')
             
             action = input("Please enter your command: ")
@@ -22,8 +30,13 @@ class MainUI():
             if action == "1":
                 self.airplaneui.airplane_menu()
             if action == "2":
-                self.call_on_validate_and_change()
+                pass
+                # self.destinationui.destination_menu()
             if action == "3":
                 pass
+                # self.employee.employee_menu()
+            if action == "4":
+                pass
+                # self.voyageui.voyage_menu()
             if action == "q":
                 break
