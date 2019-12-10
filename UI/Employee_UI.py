09328e53cc7b924bc4631c9688eba54a7061f07c
arnare19 <arnare19@ru.is>
2019-12-10 16:39:07 +0000

    update

--- UI/Employee_UI.py
+++ UI/Employee_UI.py
@@ -112,74 +112,9 @@ class EmployeeUI():
 
 
     def call_on_validate_and_change(self):
-        action = ""
-        changed_employee = ""
-        print("\tChange Employee Menu")
+        self. employeeio.load_employee_from_file("alpha")
+        print(self.employeeio)
         print()
-        print('The following actions are possible:')
-        print('\t1. Create a new employee within the system.')
-        print('\t2. Change employees already within the system.')
-        print('\t3. Display employees within the system.')
-        print('Enter "b" to go back and "q" to got to the main menu.')
-        print('Enter "b" to go back and "q" to got to the main menu.')
-        while action.lower() != 'q':
-            action = input("Enter the name: ")
-            while not self.employeell.validate_name(action):
-                print("Input is invalid!")
-                action = input("Enter the name: ")
-            new_employee += action + ","
-            if action == 'b':
-                break
-            elif action == 'q':
-                return 'q'
-            action = input("Enter the ssn: ")
-            while not self.employeell.validate_ssn(action):
-                print("Input is invalid!")
-                action = input("Enter the ssn: ")
-            new_employee += action + ","
-            if action == 'b':
-                break
-            if action == 'q':
-                return 'q'
-            action = input("Enter the phone number: ")
-            while not self.employeell.validate_phone_number(action):
-                print("Input is invalid!")
-                action = input("Enter the phone number: ")
-            new_employee += action + ","
-            if action == 'b':
-                break
-            if action == 'q':
-                return 'q'
-            action = input("Enter the user name: ")
-            while not self.employeell.validate_user_name(action):
-                print("Input is invalid!")
-                action = input("Enter the user name: ")
-            new_employee += action + ","
-            if action == 'b':
-                break
-            if action == 'q':
-                return 'q'
-            action = input("Enter the rank: ")
-            while not self.employeell.validate_rank(action):
-                print("Input is invalid!")
-                action = input("Enter the rank: ")
-            new_employee += action + ","
-            if action == 'b':
-                break
-            if action == 'q':
-                return 'q'
-            action = input("Enter the permit, if appropiet: ")
-            while not self.employeell.validate_permit(action):
-                print("Input is invalid!")
-                action = input("Enter the permit, if appropiet: ")
-            new_employee += action + ","
-            if action == 'b':
-                break
-            if action == 'q':
-                return 'q'
-            sub_action = input("Please enter your command: ")
-            change_employee = self.employeell.change_employee(new_employee)
-            break
     
     def show_by_status(self):
         print()
